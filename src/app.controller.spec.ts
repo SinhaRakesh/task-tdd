106eb408d9ce01e8323e023d8cc4811ecfaa6401
sinharakesh <rksinha.btech@gmail.com>
2025-04-02 16:23:40 +0530

    test case added

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -23,5 +23,21 @@ describe('AppController', () => {
     it('should return 0 : sum of numbers in "" ', () => {
       expect(appController.getSum('')).toBe(0);
     });
+    it('should return 1 : sum of numbers in "1" ', () => {
+      expect(appController.getSum('1')).toBe(1);
+    });
+    it('should return 3 : sum of numbers available in "//;\n1;2" ', () => {
+      expect(appController.getSum('//;\n1;2')).toBe(3);
+    });
+    it('should return 6 : sum of numbers available in "1\n2,3" ', () => {
+      expect(appController.getSum('1\n2,3')).toBe(6);
+    });
+    it('should return 10 : sum of numbers available in "//;\n1\n;2;3;4" ', () => {
+      expect(appController.getSum('//;\n1\n;2;3;4')).toBe(10);
+    });
+
+    it('should return 10 : sum of numbers available in "//;\n1\n;-2;3;4" ', () => {
+      expect(appController.getSum('//;\n1\n;2;3;4')).toBe(10);
+    });
   });
 });
