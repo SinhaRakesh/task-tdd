23154e0db7810f814d73803b9093ae1cbc8d346b
sinharakesh <rksinha.btech@gmail.com>
2025-04-02 15:33:19 +0530

    created getsum function with parms and return type value only definition

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getSum(numbers: string): number {
+    console.log(numbers);
+    if (!numbers) return 0; // return 0 if empty string
+
+    return 0;
   }
 }
